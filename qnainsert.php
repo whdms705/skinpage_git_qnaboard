56666583c79b98119a16aade0ad9acfbc455ba1e
eunho <whdms705@nate.com>
2015-03-06 21:03:27 +0900

    qnainsert.php 추가

--- qnainsert.php
+++ qnainsert.php
@@ -1,8 +1,14 @@
 <?
+$conn=mysql_connect('localhost','root','apmsetup') or die(mysql_error());
+mysql_select_db('skinboard',$conn);
+mysql_query("set names utf8");
+
+
 $posttitle=$_POST['posttitle'];
 $posttext=$_POST['posttext'];
 
-echo $posttitle;
-echo $posttext;
+$insert=mysql_query("insert into qnaboard(qnatitle,qnacontent)
+
+	values('$posttitle','posttext')",$conn) or die(mysql_error());
 
 ?>
