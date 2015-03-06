152f46133387d90a3f5fec8c74238df6d4c41ac9
eunho <whdms705@nate.com>
2015-03-06 20:11:34 +0900

    Q n A  기본틀

---
+++ qnainsert.php
@@ -0,0 +1,8 @@
+<?
+$posttitle=$_POST['posttitle'];
+$posttext=$_POST['posttext'];
+
+echo $posttitle;
+echo $posttext;
+
+?>
---
+++ qnawrite.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+	<meta charset="utf-8">
+	<title>Q & A page</title>
+	<link rel="stylesheet" type="text/css" href="qna.css">
+	</head>
+	<body>
+	<form action="qnainsert.php" method="post">
+	<center>
+	<input class="posttitle"  type="text" name="posttitle" placeholder="포스트의 제목 입력하세요" ><br><br>
+	<textarea class="posttext" name="posttext"></textarea><br><br>
+	<input type="submit">
+	</center>
+	</form>
+
+	</body>
+</html>
