57239dd25413679d1559832260531b8cadd53af4
eunho <whdms705@nate.com>
2015-03-08 14:40:19 +0900

    qna 상세 내용 페이지 추가

---
+++ qnalistdetail.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Q & A detail_list</title>
+	<link rel="stylesheet" type="text/css" href="qna.css">
+</head>
+<body>
+	<form action="" method="post">
+
+
+		<center>
+			<table>
+				<?
+
+				echo 
+				"<tr>
+
+				<td></td>
+
+			    </tr>
+
+			    <tr>
+
+				<td></td>
+				
+			    </tr>";
+			?>
+		</table>
+	</center>
+
+
+</form>
+
+</body>
+</html>
