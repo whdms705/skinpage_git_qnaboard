e92d416dc0f494989a6f956ca5c1d7058cffcaf4
eunho <whdms705@nate.com>
2015-03-08 16:09:40 +0900

    수정페이지 틀 추가

---
+++ modify.php
@@ -0,0 +1,18 @@
+
+<html>
+	<head>
+	<meta charset="utf-8">
+	<title>MODIFY page</title>
+	<link rel="stylesheet" type="text/css" href="qna.css">
+	</head>
+	<body>
+	<form action="" method="post">
+	<center>
+	<input class="posttitle"  type="text" name="modifyposttitle" placeholder="수정할 제목을 입력하세요" ><br><br>
+	<textarea class="posttext" name="modifyposttext"></textarea><br><br>
+	<input type="submit">
+	</center>
+	</form>
+
+	</body>
+</html>
